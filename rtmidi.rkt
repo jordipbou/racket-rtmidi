0414a421ba22cd62b8dd1336e7b28a96bb7260b6
Jordi P. Bou <jordipbou@gmail.com>
2019-09-03 00:36:25 +0200

    Creating binding

--- rtmidi.rkt
+++ rtmidi.rkt
@@ -1,9 +1,35 @@
 #lang racket
 (require ffi/unsafe)
 
-(define rtmidi-lib (ffi-lib "C:/labs/racket/rtmidi-4.0.0/cmake-build/Debug/rtmidi.dll" #f))
+(define rtmidi-lib (ffi-lib "binaries/x64/rtmidi.dll" #f))
 
+(define RtMidiApi
+  (_enum '(RTMIDI_API_UNSPECIFIED
+           RTMIDI_API_MACOSX_CORE
+           RTMIDI_API_LINUX_ALSA
+           RTMIDI_API_UNIX_JACK
+           RTMIDI_API_WINDOWS_MM
+           RTMIDI_API_RTMIDI_DUMMY
+           RTMIDI_API_NUM)))
 
+(define RtMidiErrorType
+  (_enum '(RTMIDI_ERROR_WARNING
+           RTMIDI_ERROR_DEBUG_WARNING
+           RTMIDI_ERROR_UNSPECIFIED
+           RTMIDI_ERROR_NO_DEVICES_FOUND
+           RTMIDI_ERROR_INVALID_DEVICE
+           RTMIDI_ERROR_MEMORY_ERROR
+           RTMIDI_ERROR_INVALID_PARAMETER
+           RTMIDI_ERROR_INVALID_USE
+           RTMIDI_ERROR_DRIVER_ERROR
+           RTMIDI_ERROR_SYSTEM_ERROR
+           RTMIDI_ERROR_THREAD_ERROR))
+};
+
+(define rtmidi-get-compiled-api
+  (get-ffi-obj "rtmidi_get_compiled_api" rtmidi-lib
+               (_fun _pointer _int -> _int)))
+               
 (define in-create-default
   (get-ffi-obj "rtmidi_in_create_default" rtmidi-lib
 			   (_fun -> _pointer)))
